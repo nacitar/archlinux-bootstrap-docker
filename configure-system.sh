c3fd28cce26a4aa20453d18d32ecbf777faae5e1
Jacob McIntosh <nacitar.sevaht@gmail.com>
2021-04-30 10:03:07 -0600

    Use the vi/vim forwarder to neovim

--- configure-system.sh
+++ configure-system.sh
@@ -348,6 +348,7 @@ config_dir="$HOME/.config/nvim"
 rm -rf "$config_dir"
 mkdir -p "$config_dir"
 git clone "https://github.com/nacitar/neovim-config.git" "$config_dir"
+"$config_dir/install_home_bin_forwarder.sh"
 EOF
 ###############################
   fi
