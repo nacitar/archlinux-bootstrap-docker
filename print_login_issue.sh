367c0c1aa57eadc01d1b8ae9cdb5c4917e0ce663
Jacob McIntosh <nacitar.sevaht@gmail.com>
2025-01-31 13:35:36 -0700

    Added login issue printer.

---
+++ print_login_issue.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# NOTE: will print to the terminal weirdly due to needing to escape \ with \\\\
+# but it will work perfectly when written to /etc/issue
+echo -e '\e[H\e[2J\e[0;36m'
+echo -e '          ##'
+echo -e '         ####                       _     _ _'
+echo -e '        ######        __ _ _ __ ___| |__ | (_)_ __  _   ___  __'
+echo -e '       ########      / _` | '\''__/ __| '\''_ \\\\| | | '\''_ \\\\| | | \\\\ \\\\/ /'
+echo -e '      ##########    | (_| | | | (__| | | | | | | | | |_| |>  <'
+echo -e '     #####  #####    \\\\__,_|_|  \\\\___|_| |_|_|_|_| |_|\\\\__,_/_/\\\\_\\\\'
+echo -e '    ####      ####'
+echo -e '   #####      #####   \e[1;37mA simple, elegant gnu/linux distribution.\e[0;36m'
+echo -e '  ##              ##'
+echo -e '\e[0m'
